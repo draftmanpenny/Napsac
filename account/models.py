1db6f14de364827a7345a8dd3408557387e8c20c
Sadiki Johnson <johnsonsadiki2gmail.com>
2022-09-19 13:04:11 -0700

    log in auth completed

--- account/models.py
+++ account/models.py
@@ -1,10 +1,4 @@
+from typing_extensions import Required
 from django.db import models
 
 # Create your models here.
-
-
-class UCLA(models.Model):
-    logo = 
-    instructor = models.CharField(max_length=100)
-    textbook = models.CharField(max_length=None)
-    course = models. CharField(max_length= 100)
