0788e9ec7d950e8db566bf06c5805479e4499091
Sadiki Johnson <johnsonsadiki2gmail.com>
2022-09-19 17:42:37 -0700

    updated registration field

--- account/serializers.py
+++ account/serializers.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
 
+from account.models import Accounts
+
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
@@ -19,14 +21,16 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ('id','username','password','first_name', 'last_name', 'university', )
+        model = Accounts
+        fields = ('id','username','password', 'password2', 'first_name', 'last_name', 'university' )
         extra_kwargs = {
             'password':{'write_only': True},
         }
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'],     password = validated_data['password']  ,first_name=validated_data['first_name'],  last_name=validated_data['last_name'], university=validated_data['university'])
+        user = User.objects.create_user(validated_data['username'],     password = validated_data['password']  , password2=validated_data['password2'], first_name=validated_data['first_name'],  last_name=validated_data['last_name'], university=validated_data['university'])
         return user
+
+
 # User serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
