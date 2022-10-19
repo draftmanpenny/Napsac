b4960190e383e316d195ba300f68fd2048d73841
Sadiki Johnson <johnsonsadiki2gmail.com>
2022-10-19 13:44:48 -0700

    fixes

--- frontend/napsac/src/components/NavBar.vue
+++ frontend/napsac/src/components/NavBar.vue
@@ -1,15 +1,4 @@
 <template>
-    <div>
-        <nav>
-            <ul>
-                <li> Dashboard </li>
-                <li> Connect </li>
-                <li> Settings </li>
-                <li> Upload </li>
-                <li> About us </li>
-            </ul>
-        </nav>
-    </div>
 
 
 </template>
