a8060a2ecf32f031127512b35304dbf2f72740d4
Sadiki Johnson <johnsonsadiki2gmail.com>
2022-10-19 13:41:30 -0700

    update nav

--- frontend/napsac/src/components/NavBar.vue
+++ frontend/napsac/src/components/NavBar.vue
@@ -1,4 +1,15 @@
 <template>
+    <div>
+        <nav>
+            <ul>
+                <li> Dashboard </li>
+                <li> Connect </li>
+                <li> Settings </li>
+                <li> Upload </li>
+                <li> About us </li>
+            </ul>
+        </nav>
+    </div>
 
 
 </template>
--- frontend/napsac/src/main.js
+++ frontend/napsac/src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import jwt_decode from 'jwt-decode'
-import Vuex from 'vuex'
+
 
 createApp(App).mount('#app')
